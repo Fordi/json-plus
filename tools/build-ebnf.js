8b62e1b2428ed3e48ef075c227fd923bdd8db098
Fordi <fordiman@gmail.com>
2017-09-09 15:25:19 -0400

    Finished PEG.js -> EBNG transpiler

--- tools/build-ebnf.js
+++ tools/build-ebnf.js
@@ -4,12 +4,13 @@ function pegToEBNF(peg) {
     if (peg.type === 'grammar') {
         return {
             type: 'grammar',
-            rules: peg.rules.map(pegToEBNF)
+            rules: peg.rules.map(pegToEBNF).filter((rule) => !!rule)
         };
     }
     if (peg.type === 'rule') {
         return {
             type: 'rule',
+            name: peg.name,
             expression: pegToEBNF(peg.expression)
         };
     }
@@ -20,13 +21,13 @@ function pegToEBNF(peg) {
     if (peg.type === 'sequence') {
         return {
             type: 'sequence',
-            elements: peg.elements.map(pegToEBNF)
+            elements: peg.elements.map(pegToEBNF).filter((rule) => !!rule)
         };
     }
     if (peg.type === 'choice') {
         return {
             type: 'choice',
-            alternatives: peg.alternatives.map(pegToEBNF)
+            alternatives: peg.alternatives.map(pegToEBNF).filter((rule) => !!rule)
         };
     }
     if (peg.type === 'optional') {
@@ -48,8 +49,9 @@ function pegToEBNF(peg) {
     if (peg.type === 'literal') {
         return {
             type: 'literal',
-            value: peg.value,
-            ignoreCase: peg.ignoreCase
+            value: peg.value
+            //ignoreCase: peg.ignoreCase
+            // Figure a way to represent case insensitivity in EBNF...
         };
     }
     if (peg.type === 'zero_or_more') {
@@ -82,9 +84,165 @@ function pegToEBNF(peg) {
             expression: pegToEBNF(peg.expression)
         }
     }
+    if (peg.type === 'simple_not') {
+        // Skip until I know how these are done.
+        return null;
+    }
+    if (peg.type === 'any') {
+        return {
+            type: 'any'
+        };
+    }
     console.log(peg);
+}
+function ebnfToString(ast, ignoreRules, inlineRules, renameRules) {
+    ignoreRules = (ignoreRules || []).reduce((o,k) => {
+        o[k] = true;
+        return o;
+    }, {});
+    inlineRules = (inlineRules || []).reduce((o, k) => {
+        o[k] = true;
+        return o;
+    }, {});
+    var maxRuleNameLength = 0;
+    var indent = '    ';
+    var indentSans = '  ';
+    var nl = '\n';
+    var altDelim = '| ';
+    var seqDelim = ' ';
+    var oneOrMore = '+';
+    var zeroOrMore = '*';
+    var groupStart = '(';
+    var groupEnd = ')';
+    var classStart = '[';
+    var classEnd = ']';
+    var classRangeDelim = '-';
+    var optional = '?';
+    var any = '.';
+    var rules = {};
+    
+    function padString(str, len) {
+        return str + (new Array(len - str.length + 1).join(' '));
+    }
+    function escapeString(str) {
+        return str.split('').map((char) => {
+            if (char === '\r') { return '#xA'; }
+            if (char === '\n') { return '#xD'; }
+            if (char === '\t') { return '#x9'; }
+            var code = char.charCodeAt(0);
+            if (code >= 0x20 && code <= 0xD7FF) { return char; }
+            if (code >= 0xe000 && code <= 0xFFFD) { return char; }
+            if (code >= 0x10000 && code <= 0x10FFFF) { return char; }
+            return '#x' + code.toString(16);
+        }).join('');
+    }
+    var handlers = {
+        grammar: (node) => {
+            node.rules.forEach((rule) => {
+                if (ignoreRules[rule.name]) { return; }
+                if (inlineRules[rule.name]) {
+                    inlineRules[rule.name] = rule;
+                    ignoreRules[rule.name] = true;
+                }
+                maxRuleNameLength = Math.max(maxRuleNameLength, rule.name.length);
+            });
+            return node.rules.filter((rule) => !ignoreRules[rule.name]).map(invokeHandler).filter((exp) => !!exp).join('\n\n');
+        },
+        rule: (node) => {
+            var expression = invokeHandler(node.expression);
+            var name = node.name;
+            if (renameRules[name]) {
+                name = renameRules[name];
+            }
+            if (!expression) { return; }
+            if (node.expression.type === 'choice') {
+                expression = expression.substr(1, expression.length - 2).substr(indent.length);
+                return name + nl + indentSans + '::=' + expression;
+            }
+            if (expression.indexOf(nl) !== -1) {
+                return name + nl + indentSans + '::= ' + expression.split(nl).join(nl + indent);
+            }
+            return padString(name, maxRuleNameLength) + " ::= " + expression;
+        },
+        choice: (node) => {
+            return groupStart + nl + indent + node.alternatives.map(invokeHandler).filter((exp) => !!exp).join(nl + indent + altDelim) + nl + groupEnd;
+        },
+        sequence: (node) => {
+            return groupStart + node.elements.map(invokeHandler).filter((exp) => !!exp).join(seqDelim) + groupEnd;
+        },
+        one_or_more: (node) => {
+            var exp = invokeHandler(node.expression);
+            if (!exp) { return; }
+            return exp + oneOrMore;
+        },
+        zero_or_more: (node) => {
+            var exp = invokeHandler(node.expression);
+            if (!exp) { return; }
+            return exp + zeroOrMore;
+        },
+        optional: (node) => {
+            var exp = invokeHandler(node.expression);
+            if (!exp) { return; }
+            return exp + optional;
+        },
+        literal: (node) => {
+            return JSON.stringify(node.value);
+        },
+        group: (node) => {
+            var exp = invokeHandler(node.expression);
+            if (!exp) { return; }            
+            return groupStart + exp + groupEnd;
+        },
+        "class": (node) => {
+            return classStart + (node.inverted ? '^' : '') + escapeString(node.parts.map((part) => {
+                if (Array.isArray(part) && part.length === 2) {
+                    return part.join(classRangeDelim);
+                }
+                return part;
+            }).join('')) + classEnd;
+        },
+        rule_ref: (node) => {
+            var name = node.name;
+            if (renameRules[name]) {
+                name = renameRules[name];
+            }
+            if (inlineRules[node.name]) { return groupStart + invokeHandler(inlineRules[node.name].expression) + groupEnd; }
+            if (ignoreRules[node.name]) { return; }
+            return name;
+        },
+        any: (node) => { return any; }
+    };
+    function invokeHandler(ast) {
+        if (!handlers[ast.type]) {
+            var json = JSON.stringify(ast);
+            console.warn("Unhandled AST node: " + json);
+            return json;
+        }
+        return handlers[ast.type](ast);
+    }
+    return invokeHandler(ast);
 }
 fs.readFile('lib/json-plus-grammar.pegjs', { encoding: 'utf-8' }, function (err, grammar) {
     var ast = pegjs.parser.parse(grammar);
-    console.log(JSON.stringify(pegToEBNF(ast), null, 4));
+    var ebnfAst = pegToEBNF(ast);
+    var ebnfString = ebnfToString(ebnfAst, [ "S", "_", "String" ], [
+        'IdentifierStart', 
+        'IdentifierChar',
+        'Sign',
+        'NumberList',
+        'FractionalPart',
+        'ExponentPart',
+        'NumberList',
+        'Boolean',
+        'Null',
+        'CodeEscape',
+        'UnicodeEscape',
+        'ElementList',
+        'ElementListTail',
+        'PropertyName',
+        'PropertyLabel',
+        'NameValuePair',
+        'NameValuePairList'
+    ], { JSON: "Value", StringValue: "String" });
+    fs.writeFile('docs/json-plus.ebnf', ebnfString, { encoding: 'utf-8' }, function (err) {});
 });
--- tools/create-docs.md
+++ tools/create-docs.md
@@ -1,17 +1,28 @@
 ### How to create railroad diagrams for JSON+
 
-The stuff in this directory is for generating railroad diagrams from my 
-simplified EBNF description of JSON+.  I don't have CLI generation of the 
-railroad diagrams for this yet, so if you want to hack on the EBNF, you're 
-stuck with this for now.
+The stuff in this directory is for generating railroad diagrams from the PEG.js
+grammar.  I don't have CLI generation of the railroad diagrams for this yet, so 
+if you want to hack on the syntax and get clear railroad diagrams as feedback, 
+you're stuck with this for now.
 
 Incidentally, this tool is a damn miracle for writing a parser.
+
+
+* From the project root, run `npm install`
+* From the same location, run `node tools/build-ebnf.js`
+    
+    This will parse the PEG.js grammar for JSON+, and translate it into a 
+    simplified EBNF syntax that the next step will use.
 
 * Browse to: http://www.bottlecaps.de/rr/ui
 * Upload docs/json-plus.ebnf
 * Click "View Diagram"
 * Click "Download"
 * Open the downloaded "json-plus.xhtml"
+
+    The output from this is nice, but I like having things split apart and much 
+    simpler, so...
+
 * Copy and paste tools/postprocess.js into your browser's dev console
 * Move the downloaded files to docs/
 * Run tools/format.sh
@@ -19,6 +30,6 @@ Incidentally, this tool is a damn miracle for writing a parser.
 ### TODO
 
 * See if the railroad diagram generator has a node lib
-* Write something to translate the PEG grammar to EBNF, 
-    with some kind of filtering mechanism so I can simplify out whitespace
-
+* The stylesheets are a pain, and splitting them out from the SVG was a mistake
+    made in the spirit of best practices. The SVG files would be more reusable if
+    standalone.
