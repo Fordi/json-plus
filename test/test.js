bb92cdfbbf8664623c8ab2f2209fe9cb43642f4b
Fordi <fordiman@gmail.com>
2017-09-08 22:40:57 -0400

    Added postprocessing, type handling, and multivalue handling

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 const jsonPlus = require('../lib/json-plus');
 const fs = require('fs');
 fs.readFile('input.json', { encoding: 'utf-8' }, (err, input) => {
-	var output = JSON.stringify(jsonPlus.parse(input), null, 4).trim();
+	function Type() {
+		this.type = "Type";
+	}
+	
+	var handlers = {
+		multiValue: jsonPlus.multiValueHandler,
+		Type: (data) => new Type(data)
+	};
+	var output = JSON.stringify(jsonPlus.parse(input, handlers), null, 4).trim();
 	fs.readFile('expected.json', { encoding: 'utf-8' }, (err, expected) => {
 		if (expected.trim() !== output) {
 			console.log(expected.trim().length, output.length);
