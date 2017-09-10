fc25c2ea1da6419853ce290040873b6af405e997
Fordi <fordiman@gmail.com>
2017-09-10 11:23:57 -0400

    Rewrote test script

--- test/test.js
+++ test/test.js
@@ -1,49 +1,117 @@
-const jsonPlus = require('../lib/json-plus');
+const jsonPlus = require(__dirname + '/../lib/json-plus');
+const io = require(__dirname + '/../tools/io-tools');
+
 const fs = require('fs');
-fs.readFile('input.json', { encoding: 'utf-8' }, (err, input) => {
-	function Type() {
-		this.type = "Type";
-	}
-	Type.prototype.toJsonPlus = function (indent) {
-		return "Type " + JSON.stringify(this, indent);
-	};
-	var handlers = {
-		multiValue: jsonPlus.multiValueHandler,
-		Type: (data) => new Type(data)
-	};
-	var parsed = jsonPlus.parse(input, handlers);
-	if (parsed.reference !== parsed.label) {
-		throw new Error("Test failed; referenced object not same as labeled");
-	}
-	if (!(parsed.repeatedNames.name instanceof jsonPlus.MultiValue)) {
-		throw new Error("Test failed; repeated names failed to result in a MultiValue");
-	}
-	if (parsed.repeatedNames.name.valueOf() !== "Fordiman") {
-		throw new Error("Test failed; repeated names does not result in last name for valueOf()");
-	}
-	if (parsed.repeatedNames.name.length !== 2) {
-		throw new Error("Test failed; MultiValue failed to enumerate all listed values");
-	}
-	if (parsed.keywords[3] !== Infinity || parsed.keywords[4] !== -Infinity || !(typeof parsed.keywords[5] !== 'number' || isNaN(parsed.keywords[5]))) {
-		throw new Error("Parsing of JSON5 special numbers failed!");
-	}
-	var output = JSON.stringify(parsed, null, 4).trim();
-	fs.readFile('expected.json', { encoding: 'utf-8' }, (err, expected) => {
-		if (expected.trim() !== output) {
-			fs.writeFile("actual.json", output, { encoding: 'utf-8' }, (err) => {
-				throw new Error("Test failed; compare test/expected.json to test/actual.json");
-			});
-		} else {
-			console.log("Parse passed");
+
+function Type() {
+	this.type = "Type";
+}
+Type.prototype.toJsonPlus = function (indent) {
+	return "Type " + JSON.stringify(this, indent);
+};
+
+var handlers = {
+	multiValue: jsonPlus.multiValueHandler,
+	Type: (data) => new Type(data)
+};
+
+function compareObjects(left, right) {
+	if (typeof left !== typeof right) { throw new Error("Objects not of same type: " + typeof left + " !== " + typeof right); }
+	if (Array.isArray(left) !== Array.isArray(right)) { throw new Error("One's an array, the other is not"); }
+	var mismatch;
+	if (Array.isArray(left)) {
+		if (left.length !== right.length) { throw new Error("Arrays are of differing length"); }
+		mismatch = left.findIndex((li, i) => {
+			try {
+				return !compareObjects(li, right[i])
+			} catch (e) {
+				throw new Error("At index " + i + "\n" + e.message);
+			}
+		});
+		if (-1 !== mismatch) {
+			throw new Error("left[" + mismatch + "] !== right[" + mismatch + "]");
 		}
+		return true;
+	}
+	if (left === right) { return true; }
+	var leftKeys = Object.keys(left);
+	var rightKeys = Object.keys(right);
+	if (leftKeys.length !== rightKeys.length) {
+		throw new Error("Objects have different number of keys");
+	}
+	mismatch = Object.keys(left).findIndex((key) => {
+		try {
+			return !compareObjects(left[key], right[key]);
+		} catch (e) {
+			throw new Error("At key " + key + "\n" + e.message);
+		}
+	});
+	if (-1 !== mismatch) {
+		throw new Error("Object values do not match for [" + leftKeys[mismatch] + "]");
+	}
+	mismatch = Object.keys(right).findIndex((key) => {
+		try {
+			return !compareObjects(left[key], right[key]);
+		} catch (e) {
+			throw new Error("At key " + key + "\n" + e.message);
+		}
+	});
+	if (-1 !== mismatch) {
+		throw new Error("Object values do not match for [" + rightKeys[mismatch] + "]");
+	}
+	return true;
+}
+
+io.readFiles({
+	input: __dirname + '/input.jp',
+	expected: __dirname + '/expected.json'
+}).then(function (res) {
+	try {
+		var parsed = jsonPlus.parse(res.input, handlers);
+		var expected = JSON.parse(res.expected);
+		// Ad-hoc tests
+		if (parsed.reference !== parsed.label) {
+			throw new Error("Test failed; referenced object not same as labeled");
+		}
+		if (parsed.recursion !== parsed.recursion.recursion) {
+			throw new Error("Test failed; recursion doesn't work");
+		}
+		if (!(parsed.repeatedNames.name instanceof jsonPlus.MultiValue)) {
+			throw new Error("Test failed; repeated names failed to result in a MultiValue");
+		}
+		if (parsed.repeatedNames.name.valueOf() !== "Fordiman") {
+			throw new Error("Test failed; repeated names does not result in last name for valueOf()");
+		}
+		if (parsed.repeatedNames.name.length !== 2) {
+			throw new Error("Test failed; MultiValue failed to enumerate all listed values");
+		}
+		if (parsed.json5Keywords[0] !== Infinity || parsed.json5Keywords[1] !== -Infinity || !(typeof parsed.json5Keywords[2] !== 'number' || isNaN(parsed.json5Keywords[3]))) {
+			throw new Error("Parsing of JSON5 special numbers failed!");
+		}
+		// Compare with expected values
+		var rootKeys = Object.keys(expected);
+		var mismatch = rootKeys.findIndex((key) => {
+			try {
+				return !compareObjects(expected[key], parsed[key])
+			} catch (e) {
+				throw new Error("At key: " + key);
+			}
+		});
+		if (-1 !== mismatch) {
+			throw new Error("Mismatch with expected values");
+		}
+		// Verify consistent stringification
 		var restrung = jsonPlus.stringify(parsed);
 		var reparsed = jsonPlus.parse(restrung, handlers);
 		var rerestrung = jsonPlus.stringify(reparsed);
 		if (restrung !== rerestrung) {
-			console.error("Stringify failed: ", "\n", restrung, "\n", rerestrung);
+			throw new Error("Stringify failed: ", "\n", restrung, "\n", rerestrung);
 		} else {
 			console.log("Stringify passed");
 		}
-		
-	});
+	} catch (e) {
+		console.error(e);
+	}
+}, function (err) {
+	console.error(err);
 });
--- tools/build-docs.js
+++ tools/build-docs.js
@@ -1,83 +1,16 @@
 const pegjs = require('pegjs');
 const pegTools = require(__dirname + '/peg-tools.js');
-const fs = require('fs');
 const showdown = new (require('showdown').Converter)();
 const mustache = require('mustache');
+const io = require(__dirname + '/io-tools.js');
 
-function readFiles(list) {
-    return new Promise((resolve, reject) => {
-        
-        var results = {};
-        if (Array.isArray(list)) {
-            var count = list.length;
-            list.forEach((file) => {
-                fs.readFile(file, { encoding: 'utf-8' }, function (err, result) {
-                    if (err) { reject(err); return; }
-                    count--;
-                    results[file] = result;
-                    if (count === 0) {
-                        resolve(results);
-                    }
-                });
-            });
-        } else if (typeof list === 'object') {
-            var count = Object.keys(list).length;
-            Object.keys(list).forEach((alias) => {
-                var file = list[alias];
-                fs.readFile(file, { encoding: 'utf-8' }, function (err, result) {
-                    if (err) { reject(err); return; }
-                    count--;
-                    results[alias] = result;
-                    if (count === 0) {
-                        resolve(results);
-                    }
-                });
-                
-            });
-        } else {
-            throw new Error("BAD argument!  No file loading!");
-        }
-    });
-}
-function Writer() {
-    this.count = 0;
-    this.written = 0;
-    this.next = [];
-    this.fail = [];
-}
-Writer.prototype = {
-    write: function (fileName, data) {
-        this.count++;
-        fs.writeFile(fileName, data, { encoding: 'utf-8' }, (err) => {
-            if (err) {
-                this.fail.forEach((rejector) => {
-                    rejector(err);
-                });
-            }
-            this.written++;
-            if (this.written === this.count) {
-                this.next.forEach((resolver) => {
-                    resolver();
-                });
-            }
-        });
-    },
-    then: function (resolved, rejected) {
-        resolved && this.next.push(resolved);
-        rejected && this.fail.push(rejected);
-        return new Promise((resolve, reject) => {
-            this.next.push(resolve);
-            this.fail.push(reject);
-        });
-    }
-}
-var writer = new Writer();
+var writer = new io.Writer();
 writer.then(() => {
     console.log("Build finished");
 }, (err) => {
     console.error(err);
 });
-readFiles({
+io.readFiles({
     grammar: 'lib/json-plus-grammar.pegjs', 
     svgCss: 'tools/railroad.css', 
     docTemplate: 'tools/doc-template.html', 
